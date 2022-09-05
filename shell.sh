7482ea9f448868895f9ffee211412e1d9acc62b3
Yewon Kim <15123020+SahrulGamerz@users.noreply.github.com>
2022-09-05 17:33:33 +0800

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -8,7 +8,16 @@ echo "* To exit please enter exit."
 echo "* If you stuck inside a program, please restart the container."
 echo "*************************************************************"
 
-script -I /dev/null -O /dev/null -B /dev/null -q
+scriptVer=$(script -V)
+
+if [[ "$scriptVer" == "script from util-linux 2.32.1" ]]; then
+    script -I /dev/null -O /dev/null -B /dev/null -q
+elif [[ "$scriptVer" == "script from util-linux 2.38" ]]; then
+    script -I /dev/null -O /dev/null -B /dev/null -q
+else
+    script -I /dev/null -O /dev/null -B /dev/null -q
+fi
+
 
 # Empty line
 echo " "
