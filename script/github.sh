ff8ad421c3f32c7c4879de82bff5ada193a8b6ff
Ubuntu <sahrularsad@gmail.com>
2022-11-10 10:39:30 +0000

    Removed while loop again. editted wrong file before

--- script/github.sh
+++ script/github.sh
@@ -31,93 +31,87 @@ if [[ "$GIT_AUTO_PULL" == "TRUE" ]]; then
         echo "* Any changes made directly to this folder and subfolder will be lost!"
         echo "*************************************************************"
 
-        while true; do
+        if [[ "$SKIP_OVERWRITE_WARNING" == "FALSE" ]]; then
+            echo "Change the setting \"SKIP OVERWRITE WARNING\" in Startup tab"
+            echo "Do you want to proceed? (y/n)"
+            read -p "Answer: " yn
+        else
+            echo " "
+            echo "*************************************************************"
+            echo "* Skipping overwrite warning"
+            echo "* Change the setting \"SKIP OVERWRITE WARNING\" in Startup tab"
+            echo "*************************************************************"
+            yn=y
+        fi
 
-            if [[ "$SKIP_OVERWRITE_WARNING" == "FALSE" ]]; then
-                echo "Change the setting \"SKIP OVERWRITE WARNING\" in Startup tab"
-                echo "Do you want to proceed? (y/n)"
-                read -p "Answer: " yn
-            else
-                echo " "
-                echo "*************************************************************"
-                echo "* Skipping overwrite warning"
-                echo "* Change the setting \"SKIP OVERWRITE WARNING\" in Startup tab"
-                echo "*************************************************************"
-                yn=y
-            fi
-
-            case $yn in 
-                [nN] ) echo "Skipping...";
-                    break;;
-                [yY] ) 
-                    GITHUB_REPO=${GITHUB_REPO#*//}
-                    if [[ $GITHUB_REPO = *.git ]]; then
-                        if [ "$GITHUB_USERNAME" == "" ] && [ "$GITHUB_TOKEN" == "" ]; then
-                            git remote set-url origin "https://$GITHUB_REPO"
-                        else 
-                            git remote set-url origin "https://$GITHUB_USERNAME:$GITHUB_TOKEN@$GITHUB_REPO"
-                        fi
-                    else
-                        if [ "$GITHUB_USERNAME" == "" ] && [ "$GITHUB_TOKEN" == "" ]; then
-                            git remote set-url origin "https://$GITHUB_REPO.git"
-                        else 
-                            git remote set-url origin "https://$GITHUB_USERNAME:$GITHUB_TOKEN@$GITHUB_REPO.git"
-                        fi
+        case $yn in 
+            [nN] ) echo "Skipping...";
+                break;;
+            [yY] ) 
+                GITHUB_REPO=${GITHUB_REPO#*//}
+                if [[ $GITHUB_REPO = *.git ]]; then
+                    if [ "$GITHUB_USERNAME" == "" ] && [ "$GITHUB_TOKEN" == "" ]; then
+                        git remote set-url origin "https://$GITHUB_REPO"
+                    else 
+                        git remote set-url origin "https://$GITHUB_USERNAME:$GITHUB_TOKEN@$GITHUB_REPO"
                     fi
+                else
+                    if [ "$GITHUB_USERNAME" == "" ] && [ "$GITHUB_TOKEN" == "" ]; then
+                        git remote set-url origin "https://$GITHUB_REPO.git"
+                    else 
+                        git remote set-url origin "https://$GITHUB_USERNAME:$GITHUB_TOKEN@$GITHUB_REPO.git"
+                    fi
+                fi
 
-                    git fetch --all
-                    git reset --hard origin/$GITHUB_BRANCH;;
-                * ) echo "Invalid Response";;
-            esac
+                git fetch --all
+                git reset --hard origin/$GITHUB_BRANCH;;
+            * ) echo "Invalid Response";;
+        esac
 
-        done
     else 
         echo " "
         echo "*************************************************************"
         echo "* Pulling repository..."
         echo "* Any changes made directly to this folder and subfolder will be lost!"
         echo "*************************************************************"
-        
-        while true; do
-        
-            if [[ "$SKIP_OVERWRITE_WARNING" == "FALSE" ]]; then
-                echo "Change the setting \"SKIP OVERWRITE WARNING\" in Startup tab"
-                echo "Do you want to proceed? (y/n)"
-                read -p "Answer: " yn
-            else
-                echo " "
-                echo "*************************************************************"
-                echo "* Skipping overwrite warning"
-                echo "* Change the setting \"SKIP OVERWRITE WARNING\" in Startup tab"
-                echo "*************************************************************"
-            fi
+                
+        if [[ "$SKIP_OVERWRITE_WARNING" == "FALSE" ]]; then
+            echo "Change the setting \"SKIP OVERWRITE WARNING\" in Startup tab"
+            echo "Do you want to proceed? (y/n)"
+            read -p "Answer: " yn
+        else
+            echo " "
+            echo "*************************************************************"
+            echo "* Skipping overwrite warning"
+            echo "* Change the setting \"SKIP OVERWRITE WARNING\" in Startup tab"
+            echo "*************************************************************"
+        fi
 
-            case $yn in 
-                [nN] ) echo "Skipping...";
-                    break;;
-                [yY] ) 
-                    GITHUB_REPO=${GITHUB_REPO#*//}
-                    if [[ $GITHUB_REPO = *.git ]]; then
-                        if [ "$GITHUB_USERNAME" == "" ] && [ "$GITHUB_TOKEN" == "" ]; then
-                            git clone --branch $GITHUB_BRANCH --progress "https://$GITHUB_REPO" temp
-                        else 
-                            git clone --branch $GITHUB_BRANCH --progress "https://$GITHUB_USERNAME:$GITHUB_TOKEN@$GITHUB_REPO" temp
-                        fi
-                    else
-                        if [ "$GITHUB_USERNAME" == "" ] && [ "$GITHUB_TOKEN" == "" ]; then
-                            git clone --branch $GITHUB_BRANCH --progress "https://$GITHUB_REPO.git" temp
-                        else 
-                            git clone --branch $GITHUB_BRANCH --progress "https://$GITHUB_USERNAME:$GITHUB_TOKEN@$GITHUB_REPO.git" temp
-                        fi
+        case $yn in 
+            [nN] ) echo "Skipping...";
+                break;;
+            [yY] ) 
+                GITHUB_REPO=${GITHUB_REPO#*//}
+                if [[ $GITHUB_REPO = *.git ]]; then
+                    if [ "$GITHUB_USERNAME" == "" ] && [ "$GITHUB_TOKEN" == "" ]; then
+                        git clone --branch $GITHUB_BRANCH --progress "https://$GITHUB_REPO" temp
+                    else 
+                        git clone --branch $GITHUB_BRANCH --progress "https://$GITHUB_USERNAME:$GITHUB_TOKEN@$GITHUB_REPO" temp
                     fi
+                else
+                    if [ "$GITHUB_USERNAME" == "" ] && [ "$GITHUB_TOKEN" == "" ]; then
+                        git clone --branch $GITHUB_BRANCH --progress "https://$GITHUB_REPO.git" temp
+                    else 
+                        git clone --branch $GITHUB_BRANCH --progress "https://$GITHUB_USERNAME:$GITHUB_TOKEN@$GITHUB_REPO.git" temp
+                    fi
+                fi
 
-                    for x in temp/* temp/.[!.]* temp/..?*; do
-                        if [ -e "$x" ]; then mv -- "$x" ./; fi
-                    done;;
-                * ) echo "Invalid Response";;
-            esac
+                for x in temp/* temp/.[!.]* temp/..?*; do
+                    if [ -e "$x" ]; then mv -- "$x" ./; fi
+                done;;
+            * ) echo "Invalid Response";;
+        esac
 
-        done
     fi
 else
     echo " "
